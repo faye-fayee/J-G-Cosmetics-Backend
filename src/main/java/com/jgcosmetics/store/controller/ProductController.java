7c8854a1a1d8f419acbec4b334f47d0995c346ff
Faye <fayepeteros@gmail.com>
2025-03-30 07:58:21 +0800

    Edit Product Controller

--- src/main/java/com/jgcosmetics/store/controller/ProductController.java
+++ src/main/java/com/jgcosmetics/store/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.jgcosmetics.store.controller;
 import com.jgcosmetics.store.model.Product;
 import com.jgcosmetics.store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -38,13 +35,15 @@ public class ProductController {
     }
 
     // Add a new product
-    public Product addProduct(Product product) {
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
         return productService.saveProduct(product);
     }
 
     // Delete a product by ID
-    public String deleteProduct(int productId) {
-        productService.deleteProduct(productId);
-        return "Product with id " + productId + " was deleted";
+    @DeleteMapping("{id}")
+    public String deleteProduct(@PathVariable int id) {
+        productService.deleteProduct(id);
+        return "Product with id " + id + " was deleted";
     }
 }
