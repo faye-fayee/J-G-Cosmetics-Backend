5a778f20549da49341f141c063effce68c40e0f6
Faye <fayepeteros@gmail.com>
2025-03-31 03:18:07 +0800

    Add Product Description Method

--- src/main/java/com/jgcosmetics/store/repository/ProductRepo.java
+++ src/main/java/com/jgcosmetics/store/repository/ProductRepo.java
@@ -2,6 +2,7 @@ package com.jgcosmetics.store.repository;
 
 import com.jgcosmetics.store.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,14 @@ import java.util.List;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
+
+    // Select a product by category
     @Query("SELECT p FROM Product p WHERE LOWER(p.category) = LOWER(:category)")
     List<Product> findByCategory(@Param("category")String category);
+
+    // Edit the Description of a Product
+    @Modifying
+    @Query("UPDATE Product p SET p.description = :newDescription WHERE p.id = :productId")
+    void updateProductDescription(@Param("productId") Long productId, @Param("newDescription") String newDescription);
+
 }
