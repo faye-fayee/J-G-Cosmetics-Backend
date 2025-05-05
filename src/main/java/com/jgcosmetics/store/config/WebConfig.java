daff38e2ab8f0e2d6bbd309c7d3e593b56b268fc
Faye <fayepeteros@gmail.com>
2025-05-05 14:15:13 +0800

    Edit Web Config Class

--- src/main/java/com/jgcosmetics/store/config/WebConfig.java
+++ src/main/java/com/jgcosmetics/store/config/WebConfig.java
@@ -9,9 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("http://34.105.57.169")
-                .allowedOrigins("http://34.105.57.169:8080")
+                .allowedOrigins("http://127.0.0.1:5500") // Live Server
+                .allowedOrigins("http://localhost:8080") // Local HTML
+                .allowedOrigins("http://34.105.57.169") // VM External IP
+                .allowedOrigins("http://34.105.57.169:8080") // Online Server
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
